94b07ca94a853bc4b1b6724d3bad5d75bda470d2
Jefferson Santos <jeffersonsantos@MacBook-Air-de-Jefferson.local>
2026-06-15 11:29:15 -0300

    Atualiza iframe do mapa com embed exato do CATRE

--- src/components/Localizacao.tsx
+++ src/components/Localizacao.tsx
@@ -24,7 +24,7 @@ export default function Localizacao() {
           >
             <iframe
               title="Localização CATRE Penedo"
-              src="https://maps.google.com/maps?q=CATRE+Penedo+Alagoas&output=embed&hl=pt"
+              src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d582.4507878677503!2d-44.51668389252121!3d-22.44159891471872!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x9e799c8d2a5413%3A0x1824c94b20ae7ec2!2sCentro%20de%20Treinamento%20e%20Recrea%C3%A7%C3%A3o%20Adventista%20-%20ARS%20-%20Satulinna!5e0!3m2!1spt-BR!2sbr!4v1781533667553!5m2!1spt-BR!2sbr"
               width="100%"
               height="100%"
               style={{ border: 0 }}
